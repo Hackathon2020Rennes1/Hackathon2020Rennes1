3bccc68d9d6b269bd460195f9f0fb0bc65c74709
klibr007 <davidlafia@yahoo.fr>
2020-12-01 11:48:08 +0100

    json import

---
+++ importJsonToFirebase/config.js
@@ -0,0 +1,13 @@
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyDgEAq1ysRWqhVf-bgmgDeowmrqTepLRHY",
+    authDomain: "hackathon2020rennes1.firebaseapp.com",
+    databaseURL: "https://hackathon2020rennes1.firebaseio.com",
+    projectId: "hackathon2020rennes1",
+    storageBucket: "hackathon2020rennes1.appspot.com",
+    messagingSenderId: "525360444140",
+    appId: "1:525360444140:web:a93161577a37dd5a14abc9",
+    measurementId: "G-8G1Q14EV71"
+  };
+// Exports
+  module.exports = firebaseConfig;
